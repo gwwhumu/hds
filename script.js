ef044f2c457b40784bbf79cee145e551f6138906
humu <54346796+imfreemanguo@users.noreply.github.com>
2024-06-27 13:06:05 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript 处理模态框关闭
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.getElementById("modal");
-  const close = document.querySelector(".close");
-
-  close.onclick = function () {
-    modal.style.display = "none";
-  };
-});
